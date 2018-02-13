697f6ca399a6e3bb8d63e20789a40813e96549be
centixkadon <centixkadon@gmail.com>
2018-02-13 20:18:59 +0800

    Update util

--- util/time_diff.h
+++ util/time_diff.h
@@ -4,6 +4,10 @@
 
 #if 1
 
+#ifndef _TIME_DIFF_STRLEN
+#define _TIME_DIFF_STRLEN 20
+#endif
+
 #if (defined(WIN64) || defined(_WIN64) || defined(__WIN64__)) || (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__))
 #include <windows.h>
 #define TIME_A(str)          \
@@ -19,7 +23,7 @@
     QueryPerformanceFrequency(&t_##str##_c);                                              \
     t_##str = 1.0 * (t_##str##_b.QuadPart - t_##str##_a.QuadPart) / t_##str##_c.QuadPart; \
   }                                                                                       \
-  cout << #str << string(20 - t_##str##_s.size(), ' ') << " : " << t_##str << endl;
+  cout << #str << string(_TIME_DIFF_STRLEN - t_##str##_s.size(), ' ') << ": " << t_##str << endl;
 #elif defined(__linux__) || defined(__linux)
 #define TIME_A(str)            \
   struct timespec t_##str##_a; \
@@ -30,7 +34,7 @@
   string t_##str##_s = #str;                                                  \
   double t_##str = t_##str##_b.tv_sec - t_##str##_a.tv_sec +                  \
                    0.000000001 * (t_##str##_b.tv_nsec - t_##str##_a.tv_nsec); \
-  cout << #str << string(20 - t_##str##_s.size(), ' ') << " : " << t_##str << endl;
+  cout << #str << string(_TIME_DIFF_STRLEN - t_##str##_s.size(), ' ') << ": " << t_##str << endl;
 #else
 #define TIME_A(str)
 #define TIME_B(str) cout << "get time : system error." << endl;
